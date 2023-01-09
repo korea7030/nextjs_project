966016d96a065222037bcbf4c3ac987138cc5b52
korea7030 <korea7030@naver.com>
2023-01-09 23:03:11 +0900

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import '../styles/globals.css'
 import { ThemeProvider } from 'next-themes'
 
-
-export default function App({ Component, pageProps }) {
-  return (<ThemeProvider attribute="class">
-  <Component {...pageProps} />
-</ThemeProvider>)
+function MyApp({ Component, pageProps }) {
+    return(
+        <ThemeProvider attribute="class">
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
+
+export default MyApp
